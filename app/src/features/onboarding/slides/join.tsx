eba33abd796d9735477c76be69735138ca769035
codetann <thetannerthomas@gmail.com>
2022-06-18 11:37:41 -0600

    join slide logic

--- app/src/features/onboarding/slides/join.tsx
+++ app/src/features/onboarding/slides/join.tsx
@@ -1,17 +1,31 @@
 // import { PinInput } from "@components";
-import { PinInput } from "@components";
-import { HStack, PinInput as Inputs } from "@chakra-ui/react";
+import { useState } from "react";
+import { Button, PinInput } from "@components";
+import { Heading, HStack, PinInput as Inputs, Text } from "@chakra-ui/react";
+import { Slide } from "./slide";
 
 const JoinSlide = () => {
+  const [pin, setPin] = useState("");
+
   return (
-    <HStack>
-      <Inputs>
-        <PinInput />
-        <PinInput />
-        <PinInput />
-        <PinInput />
-      </Inputs>
-    </HStack>
+    <Slide>
+      <Heading>Enter Code</Heading>
+      <Text maxW="xl" textAlign="center" pt="1rem" color="whiteAlpha.700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+        atque non est quae laboriosam harum rem?
+      </Text>
+      <HStack py="4rem">
+        <Inputs onChange={(value) => setPin(value)}>
+          <PinInput />
+          <PinInput />
+          <PinInput />
+          <PinInput />
+        </Inputs>
+      </HStack>
+      <Button isDisabled={pin.length < 4} w="15rem">
+        Join
+      </Button>
+    </Slide>
   );
 };
 
--- app/src/theme/styles/button.ts
+++ app/src/theme/styles/button.ts
@@ -2,12 +2,22 @@ import { StyleConfig } from "@chakra-ui/theme-tools";
 
 const base_button: StyleConfig = {
   baseStyle: {
-    padding: ".8rem 2rem",
+    h: "3rem",
+    px: "2rem",
     rounded: "lg",
     color: "white",
     bg: "base.primary",
     transition: "all .2s",
     fontWeight: 500,
+
+    _disabled: {
+      cursor: "not-allowed",
+      opacity: 0.5,
+      _hover: {
+        opacity: 0.5,
+        transform: "none",
+      },
+    },
 
     _hover: {
       opacity: 0.9,
