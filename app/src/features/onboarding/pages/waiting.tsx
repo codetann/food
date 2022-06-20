eb1781a702a8970b9e7b9ce309f16d7238aeaa73
codetann <thetannerthomas@gmail.com>
2022-06-20 17:45:40 -0600

    update styles

--- app/src/features/onboarding/pages/waiting.tsx
+++ app/src/features/onboarding/pages/waiting.tsx
@@ -2,7 +2,7 @@ import { useStore } from "@store";
 import { Page, User } from "@components";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { VStack } from "@chakra-ui/react";
+import { Heading, VStack } from "@chakra-ui/react";
 
 function WaitingPage() {
   const store = useStore();
@@ -15,7 +15,7 @@ function WaitingPage() {
   return (
     <Page>
       <VStack w="100%" maxW="lg">
-        <h1>Waiting for people to join...</h1>
+        <Heading pb="2rem">Waiting for people to join</Heading>
         {store.users.map((user) => (
           <User name={user} />
         ))}
